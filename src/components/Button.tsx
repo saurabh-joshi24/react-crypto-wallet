4db3a700df1fd91a78c883faff5df9af40d9b77c
Saurabh Joshi <saurabh-joshi24>
2024-07-18 15:39:08 +0530

    feat(wallet): styling fix in button.tsx component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+      className="max-w-max mx-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
       onClick={onClick}
       disabled={disabled}
       type={type}
