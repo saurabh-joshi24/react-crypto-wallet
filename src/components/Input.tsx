0a5bc5c4a3c6bece4e02c9012bf75fda58094f8f
Saurabh Joshi <saurabh-joshi24>
2024-07-18 16:07:24 +0530

    feat(wallet): styling fix in Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,5 @@
 interface InputProps {
-  type: string;
+  type?: string;
   placeholder?: string;
   value: string | number;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -13,7 +13,7 @@ const Input: React.FC<InputProps> = ({
 }) => {
   return (
     <input
-      className="max-w-max border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="max-w-max mx-2 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
       type={type}
       placeholder={placeholder}
       value={value}
