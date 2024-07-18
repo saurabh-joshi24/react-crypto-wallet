599228b8cf50745de17898da3da5aaea21cb1363
Saurabh Joshi <saurabh-joshi24>
2024-07-18 15:40:05 +0530

    feat(wallet): styling fix in input.tsx component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,7 +13,7 @@ const Input: React.FC<InputProps> = ({
 }) => {
   return (
     <input
-      className="border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="max-w-max border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
       type={type}
       placeholder={placeholder}
       value={value}
