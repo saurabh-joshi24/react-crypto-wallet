fbaf16f7f9d2c1c5f84d5de017bc087959f58ee6
Saurabh Joshi <saurabh-joshi24>
2024-07-18 17:10:30 +0530

    feat(wallet): added AccountInfo.tsx component

---
+++ src/components/AccountInfo.tsx
@@ -0,0 +1,27 @@
+interface accountInfoProps {
+  account: string;
+  balance: string;
+  network: string | null;
+}
+
+const AccountInfo: React.FC<accountInfoProps> = ({
+  account,
+  balance,
+  network,
+}) => {
+  return (
+    <>
+      <p className="my-2">
+        <b>Account:</b> {account}
+      </p>
+      <p className="my-2">
+        <b>Balance:</b> {balance} ETH
+      </p>
+      <p className="my-2">
+        <b>Network:</b> {network}
+      </p>
+    </>
+  );
+};
+
+export default AccountInfo;
