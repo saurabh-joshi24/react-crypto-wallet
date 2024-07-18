d4f0f6611ad3c9116d2165f92327d2c153f328da
Saurabh Joshi <saurabh-joshi24>
2024-07-18 15:12:36 +0530

    feat(wallet): added utils/wallet.ts file with functions to handle connection and transfer

---
+++ src/utils/wallet.ts
@@ -0,0 +1,74 @@
+import { ethers } from "ethers";
+
+interface walletArgs {
+  account: string;
+  balance: string | number;
+  network: string;
+}
+
+interface transferArgs {
+  account: string;
+  recipientAddress: string;
+  amount: string | number;
+  onSuccess?: () => void;
+  onFailure?: () => void;
+}
+
+type onWalletConnect = (walletArgs: walletArgs) => void;
+
+interface connectWalletArgs {
+  method: string;
+  onWalletConnect: onWalletConnect;
+}
+
+
+
+const connectWallet = async ({
+  method,
+  onWalletConnect,
+}: connectWalletArgs) => {
+
+  if (window && window.ethereum) {
+    // @ts-ignore
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // @ts-ignore
+    await window.ethereum.request({ method });
+    const accounts = await provider.listAccounts();
+    const balance = await provider.getBalance(accounts[0]);
+    const network = await provider.getNetwork();
+    // @ts-ignore
+    onWalletConnect({
+      account: accounts[0],
+      balance: ethers.utils.formatEther(balance),
+      network: network.name,
+    });
+  } else {
+    alert("Please install MetaMask!");
+  }
+};
+
+const handleTransfer = async (transferArgs: transferArgs) => {
+  const { account, recipientAddress, amount } = transferArgs;
+
+  if (!account || !recipientAddress || !amount) return;
+  // @ts-ignore
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  const tx = {
+    to: recipientAddress,
+    // @ts-ignore
+    value: ethers.utils.parseEther(amount),
+  };
+
+  try {
+    const transaction = await signer.sendTransaction(tx);
+    await transaction.wait();
+    alert("Transaction successful!");
+  } catch (error) {
+    console.error("Transaction failed:", error);
+    alert("Transaction failed. Check console for details.");
+  }
+};
+
+export { handleTransfer, connectWallet };
