9e3b45d327264ac71edcb797643620b79e7775ae
Saurabh Joshi <saurabh-joshi24>
2024-07-19 18:53:38 +0530

    feat(wallet): added type for ethereum object in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,5 +3,6 @@ interface Window {
     ethereum?: {
       isMetaMask?: boolean;
       request?: (args: { method: string; params?: any[] }) => Promise<any>;
+      removeAllListeners: (event: string) => void
     };
   }
