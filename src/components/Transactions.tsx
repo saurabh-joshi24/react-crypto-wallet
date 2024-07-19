e581b317a3269527ea43100bf40d3135e3a6f612
Saurabh Joshi <saurabh-joshi24>
2024-07-19 12:10:33 +0530

    feat(wallet): added Transactions.tsx component for fetching transactions

---
+++ src/components/Transactions.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import DataTable from "./DataTable";
+import { TRANSACTION_COLUMNS } from "../constants/transaction";
+import { useQuery } from "@tanstack/react-query";
+import fetchTransactions from "../api/transactions";
+
+interface transactionProps {
+  address: string;
+  limit?: number;
+}
+
+const Transactions: React.FC<transactionProps> = ({ address, limit = 10 }) => {
+  const { data, isError, isPending, isSuccess } = useQuery({
+    queryKey: ["todos", address],
+    queryFn: () => fetchTransactions(address),
+  });
+
+  if (isPending) return <div> Loading Transactions ...</div>;
+
+  if (isError) return <div> Unable to fetch transactions</div>;
+
+  if (isSuccess && data.length < 1) return <div>No transactions</div>;
+
+  return (
+    <div>
+      <h2 className="text-xl my-2 font-bold">Recent Transactions</h2>
+      <DataTable
+        datalist={data.slice(0, limit)}
+        columns={TRANSACTION_COLUMNS}
+      />
+    </div>
+  );
+};
+
+export default Transactions;
