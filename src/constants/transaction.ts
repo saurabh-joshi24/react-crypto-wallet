0beca34abaf81005074609c7a5e39662a8043906
Saurabh Joshi <saurabh-joshi24>
2024-07-22 17:13:54 +0530

    feat(wallet): added more field in transaction.ts constants for transaction table

--- src/constants/transaction.ts
+++ src/constants/transaction.ts
@@ -1,11 +1,25 @@
 import { ethers } from "ethers";
+import { calculateTransactionFee, clipAddress } from "../utils/transaction";
 
 const TRANSACTION_COLUMNS = [
-    { header: "Transaction Hash", accessor: "hash" },
-    { header: "From", accessor: "from" },
-    { header: "To", accessor: "to"},
-    { header: "Value", cell: (item: any) => `${ethers.utils.formatEther(item.value)} ETH`},
-    { header: "Timestamp", cell: (item: any) => new Date(parseInt(item.timeStamp) * 1000).toLocaleString()},
-]
+  { header: "Transaction Hash", cell: (item: any) => clipAddress(item.hash) },
+  { header: "Block", accessor: "blockNumber" },
+  { header: "From", cell: (item: any) => clipAddress(item.from) },
+  { header: "To", cell: (item: any) => clipAddress(item.to) },
+  {
+    header: "Value",
+    cell: (item: any) => `${ethers.utils.formatEther(item.value)} ETH`,
+  },
+  {
+    header: "Txn Fee",
+    cell: (item: any) => calculateTransactionFee(item.gasPrice, item.gasUsed),
+  },
 
-export { TRANSACTION_COLUMNS }
+  {
+    header: "Timestamp",
+    cell: (item: any) =>
+      new Date(parseInt(item.timeStamp) * 1000).toLocaleString(),
+  },
+];
+
+export { TRANSACTION_COLUMNS };
