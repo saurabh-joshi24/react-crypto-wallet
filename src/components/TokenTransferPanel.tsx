7727591f6f175ca2275f2825ace39ede51c2f1b8
Saurabh Joshi <saurabh-joshi24>
2024-07-22 17:37:27 +0530

    feat(wallet): added logic for handling erc20 contract tokens transfer in TokenTransferPanel component

--- src/components/TokenTransferPanel.tsx
+++ src/components/TokenTransferPanel.tsx
@@ -2,13 +2,18 @@ import { useWalletContext } from "../hooks/wallet";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import Dropdown from "../components/Dropdown";
-import { TOKENS } from "../constants/token";
+import { fetchTokens } from "../api/tokens";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useMemo } from "react";
+import { getTokenBalance } from "../utils/token";
 
 interface TokenTransferPanelProps {
-  handleTokenTransfer: () => void;
+  address: string | null;
+  handleTokenTransfer: (address: string, type?: string) => void;
 }
 
 const TokenTransferPanel: React.FC<TokenTransferPanelProps> = ({
+  address,
   handleTokenTransfer,
 }) => {
   const {
@@ -16,9 +21,71 @@ const TokenTransferPanel: React.FC<TokenTransferPanelProps> = ({
     setAmount,
     recipientAddress,
     setRecipientAddress,
-    token,
-    setToken,
+    selectedToken,
+    setSelectedToken,
+    setBalance,
+    tokens,
+    setTokens,
+    network,
   } = useWalletContext();
+
+  const { data, isError, isPending, isSuccess, refetch } = useQuery({
+    queryKey: ["tokens", address],
+    queryFn: () => fetchTokens(address),
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [address]);
+
+  const memoizedTokens = useMemo(() => {
+    if (isSuccess && data && data.length) {
+      return data;
+    } else {
+      return [];
+    }
+  }, [data, isSuccess]);
+
+  useEffect(() => {
+    const allTokens = memoizedTokens;
+    setTokens((prevtokens) => prevtokens.concat(allTokens));
+  }, [memoizedTokens, setTokens]);
+
+  if (isPending) return <div> Fetching Tokens ...</div>;
+
+  if (isError) return <div> Unable to fetch tokens</div>;
+
+  const onButtonClick = () => {
+    const tokenSelected = memoizedTokens.find(
+      (item) => item.value === selectedToken
+    );
+    // if selected token is erc20 contract
+    if (
+      tokenSelected &&
+      Object.keys(tokenSelected).length &&
+      tokenSelected.type === "contract"
+    ) {
+      const { type } = tokenSelected;
+      handleTokenTransfer(selectedToken, type);
+    } else {
+      handleTokenTransfer(selectedToken);
+    }
+  };
+
+  const onDropdownChange = async (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const value = event.target.value;
+    const tokenSelected = memoizedTokens.find((item) => item.value === value);
+
+    setSelectedToken(value);
+
+    if (tokenSelected && Object.keys(tokenSelected).length) {
+      //@ts-ignore
+      const tokenBalance = await getTokenBalance(value, address);
+      setBalance(String(tokenBalance));
+    }
+  };
 
   return (
     <div>
@@ -26,19 +93,28 @@ const TokenTransferPanel: React.FC<TokenTransferPanelProps> = ({
         placeholder="Amount to send"
         value={amount}
         onChange={(e) => setAmount(e.target.value)}
+        disabled={network !== "sepolia"}
       />
       <Input
         placeholder="Recipient Address"
         value={recipientAddress}
         onChange={(e) => setRecipientAddress(e.target.value)}
+        disabled={network !== "sepolia"}
       />
-      <Dropdown
-        options={TOKENS}
-        onChange={(e) => setToken(e.target.value)}
-        value={token}
-        placeholder="Select Token"
+      {isSuccess && (
+        <Dropdown
+          options={tokens}
+          onChange={onDropdownChange}
+          value={selectedToken}
+          placeholder="Select Token"
+          disabled={network !== "sepolia"}
+        />
+      )}
+      <Button
+        disabled={network !== "sepolia"}
+        text="Send Token"
+        onClick={() => onButtonClick()}
       />
-      <Button text="Send Token" onClick={handleTokenTransfer} />
     </div>
   );
 };
