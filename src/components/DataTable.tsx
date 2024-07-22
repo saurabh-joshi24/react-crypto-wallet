5247776292bcc03d0be7f8a5be8bdf76323a603b
Saurabh Joshi <saurabh-joshi24>
2024-07-22 13:11:53 +0530

    feat(wallet): refactored datatable component

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,3 +1,6 @@
+import TableData from "./TableData";
+import TableHeading from "./TableHeading";
+
 interface RandomObject {
   [key: string]: any;
 }
@@ -8,40 +11,13 @@ interface DataTableProps {
 }
 
 const DataTable: React.FC<DataTableProps> = ({ datalist, columns }) => {
-  // this function allows to access nested value inside object like (a.nestedKeyA)
-  const getValue = (item: RandomObject, accessor: string) => {
-    return accessor
-      .split(".")
-      .reduce(
-        (obj: any, key: string | number) =>
-          obj && obj[key] !== undefined ? obj[key] : "",
-        item
-      );
-  };
-
   return (
     <table className="w-90 mx-auto border-collapse mb-20 border-1 border-gray-300">
       <thead>
-        <tr>
-          {columns.map((item, index) => (
-            <th className="bg-gray-200 p-2" key={index}>
-              {item.header}
-            </th>
-          ))}
-        </tr>
+        <TableHeading columns={columns} />
       </thead>
       <tbody>
-        {datalist.map((item, index) => (
-          <tr key={index}>
-            {columns.map((column, index) => (
-              <td className="border-b border-gray-300 p-4" key={index}>
-                {column.cell
-                  ? column.cell(item)
-                  : getValue(item, column.accessor)}
-              </td>
-            ))}
-          </tr>
-        ))}
+        <TableData datalist={datalist} columns={columns} />
       </tbody>
     </table>
   );
