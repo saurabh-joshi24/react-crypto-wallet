0dfaced3e81e51de162db473c848d32f36068138
Saurabh Joshi <saurabh-joshi24>
2024-07-22 15:23:11 +0530

    feat(wallet): added env.ts for enviroment variables

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+const env = {
+    ETHERSCAN: {
+        BASE_URL: process.env.REACT_APP_ETHERSCAN_API_BASE_URL,
+        API_KEY: process.env.REACT_APP_ETHERSCAN_API_KEY
+    }
+}
+
+export { env };
