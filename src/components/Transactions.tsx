6bbaa4f2fe27cd49947859d564ad3223732168aa
Saurabh Joshi <saurabh-joshi24>
2024-07-22 17:12:37 +0530

    feat(wallet): implemented pagination for transaction table

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,19 +1,55 @@
-import React from "react";
-import DataTable from "./DataTable";
+import { useState, useEffect } from "react";
+import DataTable from "./Table";
 import { TRANSACTION_COLUMNS } from "../constants/transaction";
+import {
+  INITIAL_PAGINATION_DATA,
+  PaginationType,
+} from "../constants/pagination";
 import { useQuery } from "@tanstack/react-query";
 import fetchTransactions from "../api/transactions";
+import Pagination from "./Pagination";
 
 interface transactionProps {
-  address: string |  null;
-  limit?: number;
+  address: string | null;
 }
 
-const Transactions: React.FC<transactionProps> = ({ address, limit = 10 }) => {
+const Transactions: React.FC<transactionProps> = ({ address }) => {
+  const [paginationData, setPaginationData] = useState({...INITIAL_PAGINATION_DATA });
+  const [totalPages, setTotalPages] = useState<number>(0)
+  const { itemsPerPage, indexOfFirstItem, indexOfLastItem } = paginationData;
+
   const { data, isError, isPending, isSuccess } = useQuery({
     queryKey: ["transactions", address],
-    queryFn: () => fetchTransactions(address) 
+    queryFn: () => fetchTransactions(address),
   });
+
+  useEffect(() => {
+    setPaginationData(INITIAL_PAGINATION_DATA);
+    setTotalPages(0);
+  }, [address])
+
+  useEffect(() => {
+    // if api call is success then pagination data
+    if (isSuccess && data && data.length) {
+      const totalPagesCount = Math.ceil(
+        data.length / INITIAL_PAGINATION_DATA.itemsPerPage
+      );
+      setPaginationData({ ...paginationData });
+      setTotalPages(totalPagesCount)
+    }
+  }, [data, isSuccess]);
+
+  useEffect(() => {
+    if (data && data.length) {
+      const totalPagesCount = Math.ceil(data.length / itemsPerPage);
+      setPaginationData({ ...paginationData });
+      setTotalPages(totalPagesCount)
+    }
+  }, [itemsPerPage, data, setPaginationData]);
+
+  const onPaginationChange = (pageData: PaginationType) => {
+    setPaginationData(pageData);
+  };
 
   if (isPending) return <div> Loading Transactions ...</div>;
 
@@ -23,11 +59,12 @@ const Transactions: React.FC<transactionProps> = ({ address, limit = 10 }) => {
 
   return (
     <div>
-      <h2 className="text-xl my-2 font-bold">Recent Transactions</h2>
+      <h2 className="text-xl my-4 font-bold">Recent Transactions</h2>
       <DataTable
-        datalist={data.slice(0, limit)}
+        datalist={data.slice(indexOfFirstItem, indexOfLastItem)}
         columns={TRANSACTION_COLUMNS}
       />
+      <Pagination onChange={onPaginationChange} {...paginationData} totalPages={totalPages}/>
     </div>
   );
 };
