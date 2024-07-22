222696243fc932175d15b5ee99af55799df15d9d
Saurabh Joshi <saurabh-joshi24>
2024-07-22 13:23:18 +0530

    feat(wallet): fixed value formatting for value field for transaction table

--- src/constants/transaction.ts
+++ src/constants/transaction.ts
@@ -1,8 +1,10 @@
+import { ethers } from "ethers";
+
 const TRANSACTION_COLUMNS = [
     { header: "Transaction Hash", accessor: "hash" },
     { header: "From", accessor: "from" },
     { header: "To", accessor: "to"},
-    { header: "Value", accessor: "value" },
+    { header: "Value", cell: (item: any) => `${ethers.utils.formatEther(item.value)} ETH`},
     { header: "Timestamp", cell: (item: any) => new Date(parseInt(item.timeStamp) * 1000).toLocaleString()},
 ]
 
