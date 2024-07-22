0fd24697524254cd42ce3002defddd29eb2bae0b
Saurabh Joshi <saurabh-joshi24>
2024-07-22 16:01:36 +0530

    feat(wallet): added few more props in Input.tsx component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,38 @@
 interface InputProps {
+  id?: string;
   type?: string;
   placeholder?: string;
   value: string | number;
+  className?: string;
+  step?: number;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
 const Input: React.FC<InputProps> = ({
+  id,
   type = "text",
   placeholder = "placeholder text",
   value,
+  step,
+  className,
   onChange,
+  onKeyDown,
 }) => {
   return (
     <input
-      className="max-w-max mx-2 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+      id={id}
+      className={
+        className
+          ? className
+          : "max-w-max mx-2 border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+      }
       type={type}
       placeholder={placeholder}
       value={value}
+      step={step}
       onChange={onChange}
+      onKeyDown={onKeyDown}
     />
   );
 };
